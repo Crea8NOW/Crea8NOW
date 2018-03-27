39342c3189030b69990da797c4c24fbe407f3522
Jemima Abu <jemimaabu@gmail.com>
2018-03-27 16:20:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var slideIndex = 0;
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("imgslide");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2500);
+}
+showDivs(slideIndex);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("imgslide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
